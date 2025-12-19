7ed3865e4ea64d5b2884e701257df22e77c14bea
Mateo Morales <Moralmat76@gmail.com>
2025-12-18 20:52:32 -0600

    still working on profile

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -1,17 +1,56 @@
 package org.yearup.controllers;
 
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProfileDao;
+import org.yearup.data.UserDao;
+import org.yearup.models.Profile;
+import org.yearup.models.User;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/profile")
+@PreAuthorize("isAuthenticated")
 @CrossOrigin
 public class ProfileController {
+    private final ProfileDao profileDao;
+    private final UserDao userDao;
 
-    private ProfileDao profileDao;
+    public ProfileController(ProfileDao profileDao, UserDao userDao) {
+        this.profileDao = profileDao;
+        this.userDao = userDao;
+    }
 
-    public ProfileController(ProfileDao profileDao) { this.profileDao = profileDao;}
+
+    @GetMapping
+    public Profile getProfile(Principal principal) {
+        User user = getCurrentUser(principal);
+        Profile profile = profileDao.getUserById(user.getId());
+        if (profile == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return profile;
+    }
+
+    @PutMapping
+    public Profile updateProfile(@RequestBody Profile profile, Principal principal) {
+        User user = getCurrentUser(principal);
+
+        profileDao.update(user.getId(), profile);
+
+        return profileDao.getUserById(user.getId());
+
+    }
+
+    private User getCurrentUser(Principal principal) {
+        String username = principal.getName();
+        User user = userDao.getByUserName(username);
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Username not found");
+        }
+        return user;
+    }
 }
--- src/main/java/org/yearup/data/ProfileDao.java
+++ src/main/java/org/yearup/data/ProfileDao.java
@@ -6,4 +6,6 @@ import org.yearup.models.Profile;
 public interface ProfileDao
 {
     Profile create(Profile profile);
+    Profile getByUserId (int profileId);
+    Profile update (int userId, Profile profile);
 }
